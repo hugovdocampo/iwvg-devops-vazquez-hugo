8041d657e432e0535789d19006465675e0575c68
Hugo Vázquez Docampo <81679649+hugovazquezd@users.noreply.github.com>
2023-09-27 13:05:12 +0200

    enhanced Fraction #6

--- src/main/java/es/upm/miw/iwvg_devops/code/Fraction.java
+++ src/main/java/es/upm/miw/iwvg_devops/code/Fraction.java
@@ -57,6 +57,38 @@ public class Fraction {
         return (double) numerator / denominator;
     }
 
+    public boolean isProper() {
+        return Math.abs(numerator) < Math.abs(denominator);
+    }
+
+    public boolean isImproper() {
+        return Math.abs(numerator) >= Math.abs(denominator);
+    }
+
+    public boolean isEquivalent(Fraction other) {
+        return this.numerator * other.denominator == this.denominator * other.numerator;
+    }
+
+    public Fraction add(Fraction other) {
+        int commonDenominator = this.denominator * other.denominator;
+        int newNumerator = this.numerator * other.denominator + other.numerator * this.denominator;
+        return new Fraction(newNumerator, commonDenominator);
+    }
+
+    public Fraction multiply(Fraction other) {
+        int newNumerator = this.numerator * other.numerator;
+        int newDenominator = this.denominator * other.denominator;
+        return new Fraction(newNumerator, newDenominator);
+    }
+
+    public Fraction divide(Fraction other) {
+        if (other.numerator == 0) {
+            throw new ArithmeticException("Cannot divide by zero");
+        }
+        int newNumerator = this.numerator * other.denominator;
+        int newDenominator = this.denominator * other.numerator;
+        return new Fraction(newNumerator, newDenominator);
+    }
     @Override
     public String toString() {
         return "Fraction{" +
